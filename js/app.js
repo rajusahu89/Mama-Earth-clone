6d8a7a870ac75276f349b3a372297a99c9aa3647
Your Name <you@example.com>
2022-07-10 22:47:44 +0530

    done

---
+++ js/app.js
@@ -0,0 +1,47 @@
+$(document).ready(function()
+{
+    $('.load_slider1').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn1",
+        nextArrow:".next-btn1"
+    })
+})
+
+
+$(document).ready(function()
+{
+    $('.load_slider2').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn2",
+        nextArrow:".next-btn2"
+    })
+})
+
+$(document).ready(function()
+{
+    $('.load_slider3').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn3",
+        nextArrow:".next-btn3"
+    })
+})
+
+
+$(document).ready(function()
+{
+    $('.load_slider5').slick({
+        autoplay:true,
+        slidesToShow:4,
+        slidesToScroll:1,
+        prevArrow:".prev-btn5",
+        nextArrow:".next-btn5"
+    })
+})
+
+
